de0b2746fb1a37b52f6d4b892cde46bd56ef9214
ali mohammadinai <alimohammadinia314@yahoo.com>
2022-05-31 18:21:52 +0430

    todolist react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TodoInput from './component/TodoInput';
+import "./index.css";
+import ToastContainer1 from './component/ToastContainer';
+
+
+
+function App() {
+  return (
+    <>
+    <ToastContainer1 />
+        <TodoInput />
+    </>
+  );
+}
+
+
+export default App
